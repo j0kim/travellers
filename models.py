feeb3347a08e4ff3bccbcc5f588cdbd32dc3c3d3
j0kim <joachim.wat@gmail.com>
2021-11-15 15:49:34 +0300

    add models.py and update main.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+
+class User (BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name: str
+    user_email: str
+    gender: Gender
+    country: str
+    roles: List[Role]
+
+
